e0950b389f15f06c282b4c337832c87730e4a0fb
0xh7ml <0xh7ml.py@gmail.com>
2025-05-16 18:26:49 +0600

    API Root added

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 import { create } from "domain"
 
-export const API_ROOT = process.env.NEXT_PUBLIC_API_ROOT || 'http://localhost:3000/api/v1/'
+export const API_ROOT = process.env.NEXT_PUBLIC_API_ROOT || 'https://dtt.itsaikat.com/api/v1/'
 export const API_ENDPOINTS = {
   createPost: `${API_ROOT}posts`,
   createComment: `${API_ROOT}posts`,
